9c00d1888590c41002b772355083dc93cf46b273
s1f10220266 <s1f102202662@iniad.org>
2023-12-07 17:22:55 +0900

    add authtest, accounts app
    認証機能を持つアプリを初めから作り直しました

--- config/settings.py
+++ config/settings.py
@@ -38,6 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'teamapp',
+    'authtest',
+    'accounts',
 ]
 
 MIDDLEWARE = [
@@ -124,5 +126,6 @@ STATIC_URL = 'static/'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 LOGIN_URL = 'login'
-LOGIN_REDIRECT_URL = 'index'
-LOGOUT_REDIRECT_URL = 'index'
+LOGIN_REDIRECT_URL = 'home'
+LOGOUT_REDIRECT_URL = 'home'
+
--- config/urls.py
+++ config/urls.py
@@ -17,7 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path, include
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('app01/', include("teamapp.urls")),
+    path('authtest/', include("authtest.urls")),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
